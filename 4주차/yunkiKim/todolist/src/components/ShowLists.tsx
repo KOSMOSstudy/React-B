c3b28f22c38b8e17a69a8a33df27a3f8c6ee4738
skullKim <kyk990328@gmail.com>
2021-10-10 15:44:09 +0900

    추가: 입력한 리스트 보여지는 부분 박스 디자인 완료

---
+++ 4주차/yunkiKim/todolist/src/components/ShowLists.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const ShowListBox = styled.section`
+  height: 334px;
+  width: 657px;
+  background-color: white;
+`;
+
+const ShowLists = () => {
+    return (
+        <ShowListBox />
+    );
+}
+
+export default ShowLists;
--- 4주차/yunkiKim/todolist/src/page/TodoList.tsx
+++ 4주차/yunkiKim/todolist/src/page/TodoList.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 import Header from "../components/Header";
 import WriteList from "../components/WriteList";
+import ShowLists from "../components/ShowLists";
 
 const TodoListBox = styled.section`
   height: 483px;
@@ -18,6 +19,7 @@ const TodoList = () => {
         <TodoListBox>
             <Header />
             <WriteList />
+            <ShowLists />
         </TodoListBox>
     )
 }
